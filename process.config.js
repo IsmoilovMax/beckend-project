1172e75acfeeb0f6043fb1fbbad1db6ce144a56e
IsmoilovMax <maxmudjon2121@gmail.com>
2025-01-24 16:33:31 +0900

    fix: deploy.sh

---
+++ process.config.js
@@ -0,0 +1,11 @@
+module.export = {
+    apps :[{
+        name: "Flower_Backend",
+        cwd: './',
+        script: "./dist/server.js",
+        watch: false,
+        env_production:{
+            NODE_ENV: "production"
+        },
+    }]
+}
