dae180f3540f6233be724847b97f1c40853893e3
IsmoilovMax <maxmudjon2121@gmail.com>
2025-01-24 17:22:07 +0900

    fix: change Port

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-Production
+#Production
 git reset --hard
 git checkout master
 git pull origin master 
--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,7 @@ mongoose
 	.connect(process.env.MONGO_URL as string, {})
 	.then(data => {
 		console.log('Mongo-Db connection okey')
-		const PORT = process.env.PORT ?? 8080
+		const PORT = process.env.PORT ?? 9090
 		server.listen(PORT, function () {
 			console.info(`The server is running  successfull on port : ${PORT}`)
 			console.info(`Admin project on http://localhost:${PORT}/admin`)
